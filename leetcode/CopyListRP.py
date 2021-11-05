7af9b0da6b9c64a5d8382239bf548653941a18b7
Gianluca DelBarba <gianlucadb06@gmail.com>
2021-11-04 23:59:51 -0400

    started another problem

---
+++ leetcode/CopyListRP.py
@@ -0,0 +1,32 @@
+"""
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+"""
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x, next=None, random=None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: Node
+        :rtype: Node
+        """
+        newHead = Node(head.val, None, None)
+        prev = newHead
+        ptr = head.next
+        while ptr != None:
+            newPtr = Node(ptr.val, None, None)
+            prev.next = newPtr
+            prev = newPtr
+            ptr = ptr.next
+        return newHead
+
+#in progress
