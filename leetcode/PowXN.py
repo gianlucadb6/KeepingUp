cb8eb0d449cdaf7ecec4481873e744fe949c00a3
Gianluca DelBarba <gianlucadb06@gmail.com>
2021-11-28 20:36:44 -0500

    initial idea

---
+++ leetcode/PowXN.py
@@ -0,0 +1,17 @@
+"""
+Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+"""
+
+class Solution(object):
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n == 0:
+            return 1
+        elif n == 1:
+            return n
+        else:
+            return n*n*self.myPow(x,n-2)
